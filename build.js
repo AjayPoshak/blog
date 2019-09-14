d44e60d58571624bbfef79a33d8da9084dd17a50
Ajay Poshak <poshakajay@gmail.com>
2019-09-14 17:37:07 +0530

    Read article attributes from content.yaml and replace with placeholder in html files

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const yaml = require("js-yaml");
 const { markdown } = require("./utils/markdown.js");
 const { getListOfFiles, cleanUpDirectory } = require("./utils/fileHelpers.js");
 
@@ -9,6 +10,8 @@ nunjucks.configure("views", {
   autoescape: true
 });
 
+const contentConfig = yaml.safeLoad(fs.readFileSync("content.yaml", "utf8"));
+
 const articleFolderPath = path.join(__dirname, "/articles");
 const buildFolderPath = path.join(__dirname, "/.assets");
 
@@ -17,16 +20,25 @@ const listOfMarkdownFiles = getListOfFiles(articleFolderPath);
 cleanUpDirectory(buildFolderPath);
 
 for (const fileName of listOfMarkdownFiles) {
+  const fileConfig = contentConfig.articles.find(
+    article => article.filename === fileName
+  );
+
   const fileContent = fs.readFileSync(
     path.join(articleFolderPath, fileName),
     "utf-8"
   );
-  
+
+  const { title, publishedDate, datetime, excerpt } = fileConfig;
   const mdContent = markdown(fileContent);
   const content = nunjucks.render("index.html", {
+    title,
+    excerpt,
+    datetime,
+    publishedDate,
     partialContent: mdContent
   });
 
   fs.writeFileSync(`.assets/${fileName.slice(0, -3)}.html`, content);
-  console.log('\x1b[36m%s\x1b[32m', 'Changed content successfully');
+  console.log("\x1b[36m%s\x1b[32m", "Changed content successfully");
 }
