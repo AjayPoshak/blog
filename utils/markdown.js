a83d34568636752ec958688d8760854cf1e9db10
Ajay Poshak <poshakajay@gmail.com>
2019-09-14 16:54:42 +0530

    Add helper to transform md into html

---
+++ utils/markdown.js
@@ -0,0 +1,18 @@
+const hljs = require('highlight.js')
+
+function markdown(content) {
+  const md = require("markdown-it")({
+    highlight: function(str, lang) {
+      if (lang && hljs.getLanguage(lang)) {
+        try {
+          return hljs.highlight(lang, str).value;
+        } catch (__) {}
+      }
+
+      return "";
+    }
+  });
+  return md.render(content);
+}
+
+module.exports = { markdown };
